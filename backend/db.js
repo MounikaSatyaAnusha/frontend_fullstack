963ec664b41adfd60318ccb3e4b65a83151e9e65
Niharika1001 <thivariniharika@gmail.com>
2024-08-21 17:13:53 +0530

    changed in package.json

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 let db;
 async function connectToDB(cb) {
-    const url = "mongodb://localhost:27017"
+    const url = "mongodb+srv://thivariniharika:#Harika@1001@cluster0.twvqtbz.mongodb.net/"
     const client = new MongoClient(url);
     await client.connect();
     db = client.db("AST_Database");
