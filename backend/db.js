dd7de194d77c9992e6d5bd95764a000cdc617f63
Niharika1001 <thivariniharika@gmail.com>
2024-08-24 17:27:29 +0530

    changed code and included try catch

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,38 @@
+// import { MongoClient } from "mongodb";
+
+// let db;
+
+// async function connectToDB(cb) {
+//     const url = "mongodb+srv://thivariniharika:Harika1001@cluster0.twvqtbz.mongodb.net/"
+//     const client = new MongoClient(url);
+//     await client.connect();
+//     db = client.db("AST_Database");
+//     cb();
+
+// }
+// // connectToDB()
+
+// export { connectToDB, db };
+
+
+
 import { MongoClient } from "mongodb";
 
 let db;
-async function connectToDB(cb) {
-    const url = "mongodb+srv://thivariniharika:Harika1001@cluster0.twvqtbz.mongodb.net/"
-    const client = new MongoClient(url);
-    await client.connect();
-    db = client.db("AST_Database");
-    cb();
-}
 
-// connectToDB()
+async function connectToDB(cb) {
+    const url = "mongodb+srv://thivariniharika:Harika1001@cluster0.twvqtbz.mongodb.net/";
+
+    try {
+        const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+        db = client.db("AST_Database");
+        console.log("Connected to MongoDB successfully.");
+        cb();
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        // Optionally, handle the error further or rethrow it
+    }
+}
 
 export { connectToDB, db };
